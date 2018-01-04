71498485871b741e107cc30637c690a322bab57a
Otto Modinos <ottomodinos@gmail.com>
2018-01-04 18:01:17 +0200

    move python history with the others
    It doesn't always work though, see comments.

--- profile.d/hist.sh
+++ profile.d/hist.sh
@@ -9,3 +9,13 @@ mkdir -p "$RLWRAP_HOME"
 
 # For the 'less' pager
 export LESSHISTFILE="$HISTDIR/less"
+
+# The python interpreter runs $PYTHONSTARTUP at the, well, startup in
+# interactive mode.  It's there that we set the history file to $PYTHONHISTORY,
+# it's can't be done any other way.  This is not a complete solution: if python
+# is run with the -i flag, $PYTHONSTARTUP is not run but 'import site' is,
+# which is what loads readline and the history (only in python 3.4 and later).
+# One could write a 'usercustomize' module, however you'd need to place it
+# somewhere in the python path which is different between versions and systems.
+export PYTHONSTARTUP="$HOME/.pythonrc"
+export PYTHONHISTORY="$HISTDIR/python"
