2fa92201cd08b548c2a38d7a28e863622cd1c6b4
Otto Modinos <ottomodinos@gmail.com>
2018-06-12 19:53:13 +0300

    make elinks XDG compliant

--- profile.d/xdg-config.sh
+++ profile.d/xdg-config.sh
@@ -4,9 +4,12 @@
 # at least move if out of the way.
 export HTTPIE_CONFIG_DIR=${XDG_CONFIG_HOME:-"$HOME/.config"}/httpie
 
+# I don't care much for elinks configs either.
+export ELINKS_CONFDIR=${XDG_CONFIG_HOME:-"$HOME/.config"}/elinks
+
 # The logs, at least, the channel logs, can be set to go to $HISTDIR with
 #     /set logger.file.path "${env:HISTDIR}/weechat"
-# The program logs always go to $WEECHAT_HOME/weechat.log
+# The program logs always go to $WEECHAT_HOME/weechat.log.
 export WEECHAT_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}/weechat
 
-mkdir -p "$HTTPIE_CONFIG_DIR" "$WEECHAT_HOME"
+mkdir -p "$HTTPIE_CONFIG_DIR" "$ELINKS_CONFDIR" "$WEECHAT_HOME"
