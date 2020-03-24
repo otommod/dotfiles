756e7d8f29cacc6b5e155617f0aeb4eb60a2e016
Otto Modinos <ottomodinos@gmail.com>
2020-03-25 01:44:11 +0200

    sh: clean up some aliases
    Having an entire `if' in the alias was dumb

--- shrc.d/aliases.sh
+++ shrc.d/aliases.sh
@@ -1,30 +1,24 @@
 alias ls='ls --color=auto -Fh --group-directories-first'
 alias l='ls'
-# alias l='ls -1A'         # Lists in one column, hidden files.
 alias la='ls -A'         # Lists human readable sizes, hidden files.
 alias ll='ls -l'         # Lists human readable sizes.
 alias lla='ll -A'        # Lists human readable sizes, hidden files.
 alias lal='la -l'        # same
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
 alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 
-alias suspend='systemctl suspend; exit'
-
-# The "ternary operator", as normally written in shell,
-#       cond && if-expr || else-expr
-# has not the exact semantics as its C counterpart: if the if-expr returns
-# non-zero then else-expr will also be executed.
-#
-# If that where to happen to our alias, that is, if rlwrap exists and dash
-# exits with a non-zero code it would be executed again!  The sollution is
-# simple, just use an if.
-alias dash='if command -v rlwrap >/dev/null; then rlwrap dash; else dash; fi'
-
+# One alternative to the alias is the following function
+# dash() {
+#     if command -v rlwrap >/dev/null; then
+#         rlwrap dash "$@"
+#     else
+#         dash "$@"
+#     fi
+# }
+alias rldash='rlwrap dash'
 
 #
 # Rsync
