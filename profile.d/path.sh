673d61a701b78472b0d30cc4928b588c530f92db
Otto Modinos <ottomodinos@gmail.com>
2020-05-01 18:14:43 +0300

    shell: set $PATH a little nicer

--- profile.d/path.sh
+++ profile.d/path.sh
@@ -1,41 +1,26 @@
 #!/bin/sh
 
-__profile_path_join() {
-    for arg; do
-        [ -n "$arg" ] && printf '%s:' "$arg"
-    done
+# Taken mostly from Arch Linux's /etc/profile
+prependpath() {
+    case ":$PATH:" in
+        *:"$1":*) ;;
+        *) PATH="$1${PATH:+:$PATH}" ;;
+    esac
 }
+
+prependpath "$HOME/bin"
+prependpath "$HOME/.local/bin"
+
+command -v go >/dev/null && \
+    prependpath "${GOPATH:-$HOME/go}/bin"
+
 
 # XXX: by default python uses ~/.local/bin, so dunno if this is needed
 # if command -v python3 >/dev/null; then
-#     python -c 'import sys, site; sys.stdout.write(site.USER_BASE + "/bin")'
+#     python3 -c 'import sys, site; sys.stdout.write(site.USER_BASE + "/bin")'
 # or just
-#     "$(python -m site --user-base)/bin"
+#     "$(python3 -m site --user-base)/bin"
 # fi
 
-# TODO: consider the simpler ~/.gem/ruby/*/bin
-
-PATH="$(__profile_path_join                     \
-        "$HOME/bin"                             \
-        "$HOME/.local/bin"                      \
-        "$(
-            if command -v ruby >/dev/null; then
-                ruby -e 'print "#{Gem.user_dir}/bin"'
-            fi
-        )"                                      \
-        "$(
-            if command -v go >/dev/null; then
-                printf '%s/bin' "${GOPATH:=$HOME/go}"
-            fi
-        )"                                      \
-        "$PATH"                                 \
-)"
-
-# During command expansion, the shell strips any trailing whitespace from the
-# output of the command.  So if the last element of the existing PATH had
-# trailing spaces or newlines, we wouldn't normally know.  There is therefore
-# the common shell idiom of printing some known letter as the last output of
-# every command which is then removed.  We use a ':' in this case.
-export PATH="${PATH%:}"
-
-unset __profile_path_join
+export PATH
+unset prependpath
