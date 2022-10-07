ce00d674925091c16cfa03b55026d49ee36d8bfa
Otto Modinos <ottomodinos@gmail.com>
2022-10-07 20:35:17 +0300

    nvim: fix init.lua
    The `vim.fn.system` call was wrong and `luajit` (that neovim uses)
    requires the bitLib to handle bitwise-operators correctly

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,12 +2,19 @@ local function ensure_installed(repo_url)
   local plugin_name = vim.fn.fnamemodify(repo_url, ":t")
   local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/" .. plugin_name
   if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.system("git", "clone", repo_url, install_path)
+    vim.fn.system({"git", "clone", repo_url, install_path})
   end
 end
 
 ensure_installed("https://github.com/wbthomason/packer.nvim")
 ensure_installed("https://github.com/rktjmp/hotpot.nvim")
 
-require("hotpot")
+require("hotpot").setup {
+  compiler = {
+    modules = {
+      useBitLib = true,
+    },
+  },
+}
+
 require("rc")
